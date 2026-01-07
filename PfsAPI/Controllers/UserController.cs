71436c317bec289415f1c77d1409706d45187a64
Lucas Sidnei dos Santos <lucas@infoworker.com.br>
2026-01-07 07:49:21 -0300

    Adicionar arquivos de projeto.

---
+++ PfsAPI/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using PfsDomain.Requests;
+
+namespace PfsAPI.Controllers
+{
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [Route("auth/token")]
+        public IActionResult Login([FromBody] Login.Request login)
+        {
+            return Ok();
+        }
+    }
+}
